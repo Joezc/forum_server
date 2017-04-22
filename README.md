51173466eedcc728f860a721c3e612a6c5979279
Joezc <lzc814310057@163.com>
2017-04-22 18:52:07 +0800

    rm all

--- README.md
+++
@@ -1,8 +0,0 @@
-## forum
-
-### Done
-+ could fetch content from server, API speciafication according to hacker new API, see [here](./hacker_news_api.md)
-
-### TODO
-+ add post function
-+ add user management system
--- item/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    # ex: /item/0/
-    url(r'^(?P<item_id>[0-9]+)/$', views.detail, name='detail'),
-]
